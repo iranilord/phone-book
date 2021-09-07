10c493a7170146274f50220b3cee5f1107cd05f3
LORD <bad_boy_m7@yahoo.com>
2021-09-07 20:03:23 +0430

    create Anno to the projects :D

---
+++ src/main/java/com/lordtarh/phonebook/annotations/PrimaryKey.java
@@ -0,0 +1,13 @@
+package com.lordtarh.phonebook.annotations;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrimaryKey {
+    String name() default "";
+    int size() default 100;
+}
--- src/main/java/com/lordtarh/phonebook/model/Users.java
+++ src/main/java/com/lordtarh/phonebook/model/Users.java
@@ -1,11 +1,18 @@
 package com.lordtarh.phonebook.model;
 
+import com.lordtarh.phonebook.annotations.*;
 import lombok.Data;
 
 @Data
+@Table(name = "users")
 public class Users {
+    @Column()
+    @PrimaryKey
     private int id;
+    @Column()
     private String name;
+    @Column()
     private String family ;
+    @Column()
     private String phone;
 }
