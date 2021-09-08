a812c1d1e1585c2b6ddd9bc3f3ab8fd72f071b10
LORD <bad_boy_m7@yahoo.com>
2021-09-08 22:28:24 +0430

    Database Connections Created :D

--- src/main/java/com/lordtarh/phonebook/Main.java
+++ src/main/java/com/lordtarh/phonebook/Main.java
@@ -1,15 +1,10 @@
 package com.lordtarh.phonebook;
-
 import com.lordtarh.phonebook.database.dbConnections;
-import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            dbConnections.getConnection().createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        dbConnections.getConnection();
     }
 }
--- src/main/java/com/lordtarh/phonebook/database/dbConnections.java
+++ src/main/java/com/lordtarh/phonebook/database/dbConnections.java
@@ -1,37 +1,48 @@
 package com.lordtarh.phonebook.database;
 
-import java.io.FileInputStream;
+import com.lordtarh.phonebook.reflections.UsersReflections;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 import java.util.Properties;
 
 public class dbConnections {
     private static Connection connection;
     private static dbConnections dbConnections;
-    private String url,username,password;
-    private dbConnections(){
+    private String url, username, password;
+
+
+    private dbConnections() {
         readConfig();
-        System.out.println(url+username+password);
         getConnectiondb();
+        createDb();
+        createTable();
 
     }
 
+    public static Connection getConnection() {
 
-    private Connection getConnectiondb(){
+        synchronized (dbConnections.class) {
+            if (dbConnections == null)
+                dbConnections = new dbConnections();
+        }
+
+        return connection;
+    }
+
+    private Connection getConnectiondb() {
         try {
-            connection = DriverManager.getConnection(url,username,password);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return connection;
     }
 
-
-    private void readConfig(){
+    private void readConfig() {
         Properties properties = new Properties();
         try {
             InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("app.properties");
@@ -41,18 +52,50 @@ public class dbConnections {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        url=properties.getProperty("url");
+        url = properties.getProperty("url");
         password = properties.getProperty("password");
         username = properties.getProperty("username");
     }
 
+    private void createTable() {
+        String table = UsersReflections.getUsersCAnno();
+        String id = UsersReflections.getUsersPKanno();
+        List<String> fileds = UsersReflections.getUsersFAnno();
+        StringBuilder query =new StringBuilder("CREATE TABLE IF NOT EXISTS ");
+        query.append(table+"(");
+        if (!id.equals("")){
+            query.append(id+" int(11) NOT NULL auto_increment,");
+        }
+        for (String s :fileds) {
+            if (s.equals(id))
+                continue;
+            query.append(s+" varchar(100) NOT NULL ,");
+        }
+        query.append("PRIMARY KEY ("+id+"))");
 
 
-    public static Connection getConnection(){
-        if (dbConnections == null)
-            dbConnections = new dbConnections();
-        return connection;
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
+
+    private void createDb(){
+        Statement statement;
+        String DB = "lordPb";
+        String checkTb = "CREATE DATABASE IF NOT EXISTS " + DB;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(checkTb);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
